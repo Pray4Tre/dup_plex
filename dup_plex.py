e3532e7e6e3338eddc307146ea36388cc2924de2
Trever Ehrfurth <pray4tre@gmail.com>
2020-12-22 21:10:30 -0600

    Update and rename plex_dupefinder.py to dup_plex.py

--- dup_plex.py
+++ dup_plex.py
@@ -31,7 +31,7 @@ logging.basicConfig(
     datefmt='%H:%M:%S'
 )
 logging.getLogger('urllib3.connectionpool').disabled = True
-log = logging.getLogger("Plex_Dupefinder")
+log = logging.getLogger("dup_plex")
 
 # Setup PlexServer object
 try:
@@ -320,16 +320,16 @@ def build_tabulated(parts, items):
 
 if __name__ == "__main__":
     print("""
-       _                 _                   __ _           _
- _ __ | | _____  __   __| |_   _ _ __   ___ / _(_)_ __   __| | ___ _ __
-| '_ \| |/ _ \ \/ /  / _` | | | | '_ \ / _ \ |_| | '_ \ / _` |/ _ \ '__|
-| |_) | |  __/>  <  | (_| | |_| | |_) |  __/  _| | | | | (_| |  __/ |
-| .__/|_|\___/_/\_\  \__,_|\__,_| .__/ \___|_| |_|_| |_|\__,_|\___|_|
-|_|                             |_|
+     _                     _           
+  __| |_   _ _ __    _ __ | | _____  __
+ / _` | | | | '_ \  | '_ \| |/ _ \ \/ /
+| (_| | |_| | |_) | | |_) | |  __/>  < 
+ \__,_|\__,_| .__/  | .__/|_|\___/_/\_\
+            |_|     |_|                
 
 #########################################################################
-# Author:   l3uddz                                                      #
-# URL:      https://github.com/l3uddz/plex_dupefinder                   #
+# Author:   pray4tre                                                    #
+# URL:      https://github.com/pray4tre/dup_plex                        #
 # --                                                                    #
 #         Part of the Cloudbox project: https://cloudbox.works          #
 #########################################################################
